212060de8022da0d9935ed2d4d92e8284f75ed89
Maxime Dupré <maximedupre@me.com>
2021-06-18 20:09:30 -0400

    should have the token symbol RAILR

--- test/railr-token.js
+++ test/railr-token.js
@@ -1,6 +1,14 @@
 const RailrToken = artifacts.require("RailrToken");
 
 contract("RailrToken", (accounts) => {
+    it("should have the token symbol RAILR", async () => {
+        const instance = await RailrToken.deployed();
+
+        const symbol = await instance.symbol();
+
+        assert.equal(symbol, "RAILR5", "token symbol");
+    });
+
     it("should have the Railroad Token name", async () => {
         const instance = await RailrToken.deployed();
 
