14acd336502ad2a410971babbc58e499edc7eb19
Maxime Dupré <maximedupre@me.com>
2021-06-18 20:13:15 -0400

    tests the total token supply and decimals

--- test/railr-token.js
+++ test/railr-token.js
@@ -1,7 +1,7 @@
 const RailrToken = artifacts.require("RailrToken");
 
 contract("RailrToken", (accounts) => {
-    it("should have the token symbol RAILR", async () => {
+    it("should have the symbol RAILR", async () => {
         const instance = await RailrToken.deployed();
 
         const symbol = await instance.symbol();
@@ -14,6 +14,18 @@ contract("RailrToken", (accounts) => {
 
         const name = await instance.name();
 
-        assert.equal(name, "Railroad Token", "token name");
+        assert.equal(name, "Railroad Token", "name");
+    });
+
+    it("should have an initial total supply of 10B tokens", async () => {
+        const instance = await RailrToken.deployed();
+
+        const totalSupply = await instance.totalSupply();
+
+        assert.equal(
+            totalSupply.toString(),
+            "10000000000" + "000000000",
+            "total supply",
+        );
     });
 });
